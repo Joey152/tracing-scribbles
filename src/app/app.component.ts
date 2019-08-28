9d100113bea21622b5bc3147ab2684908bf3ecf5
Joey Chan <joeyc.152@gmail.com>
2019-08-27 21:38:58 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'trsc-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'tracing-scribbles';
+}
